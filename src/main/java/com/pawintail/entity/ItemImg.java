4ab1ddcf93485c55ed70c70cbd3c49a93548c224
PARKSOWON <87845824+sowonhope@users.noreply.github.com>
2022-06-28 13:51:51 +0900

    Update ItemImg.java

--- src/main/java/com/pawintail/entity/ItemImg.java
+++ src/main/java/com/pawintail/entity/ItemImg.java
@@ -25,28 +25,28 @@ import lombok.Setter;
 public class ItemImg extends BaseEntity{
 
 	@Id
-    @Column(name="item_img_id")
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-	
+	@Column(name="item_img_id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
 	private String imgName; //이미지 파일명
 
-    private String oriImgName; //원본 이미지 파일명
+	private String oriImgName; //원본 이미지 파일명
 
-    private String imgUrl; //이미지 조회 경로
+	private String imgUrl; //이미지 조회 경로
 
-    private String repimgYn; //대표 이미지 여부
+	private String repimgYn; //대표 이미지 여부
 
-    //아이템(부모레코드), 아이템이미지(자식레코드) cascade로 삭제 위함
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "item_id")
-    private Item item;
-    
-    public void updateItemImg(String oriImgName, String imgName, String imgUrl){
-        this.oriImgName = oriImgName;
-        this.imgName = imgName;
-        this.imgUrl = imgUrl;
-    }
+	//아이템(부모레코드), 아이템이미지(자식레코드) cascade로 삭제 위함
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "item_id")
+	private Item item;
+
+	public void updateItemImg(String oriImgName, String imgName, String imgUrl){
+	this.oriImgName = oriImgName;
+	this.imgName = imgName;
+	this.imgUrl = imgUrl;
+	}
     
     
 }
